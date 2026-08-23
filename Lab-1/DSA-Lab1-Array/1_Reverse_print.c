bde1f86639106707d255e5c237d8765927a691c8
asusdream1900-ship-it <asusdream1900@gmail.com>
2026-08-23 20:47:00 +0530

    uppload lab1 suestion 1

---
+++ Lab-1/DSA-Lab1-Array/1_Reverse_print.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+void main(){
+    
+    int arr[] = {3,6,2,66,35,10,56,11,46,99};
+
+    printf("OUTPUT- ");
+    for(int i = 9; i>=0; i--){
+        printf("%d ",arr[i]);
+    }
+}
